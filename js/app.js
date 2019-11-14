d2a51d1c1a0c7f128175f78a1c9a79efa51b6a82
moksleivis <m@u.com>
2019-11-14 18:27:02 +0200

    tasks

--- js/app.js
+++ js/app.js
@@ -1,29 +1,42 @@
+// const button = document.querySelector('button');
+// const container = document.querySelector('.container');
+//
+// const headings = document.querySelectorAll('h3');
+// const paragraphs = document.querySelectorAll('p');
+//
+// const colorBg = document.querySelector('.bg');
+// const colorHeading = document.querySelector('.heading');
+// const colorParagraph = document.querySelector('.paragraph');
+//
+//
+// button.addEventListener('click', changeColor);
+// function changeColor() {
+//     container.style.backgroundColor = colorBg.value;
+//
+//     for (item of headings) {
+//         item.style.color = colorHeading.value;
+//     }
+//     for (item of paragraphs) {
+//         item.style.color = colorParagraph.value;
+//     }
+// }
+
+//Task Manager
 const button = document.querySelector('button');
-const container = document.querySelector('.container');
+const deleteTask = document.querySelector('button:last-child');
+const newTask = document.querySelector('input');
+const tasks = document.querySelector('ul');
 
-const headings = document.querySelectorAll('h3');
-const paragraphs = document.querySelectorAll('p');
+button.addEventListener('click', addTask);
+deleteTask.addEventListener('click', deleteTheTask);
 
-const colorBg = document.querySelector('.bg');
-const colorHeading = document.querySelector('.heading');
-const colorParagraph = document.querySelector('.paragraph');
-
-
-button.addEventListener('click', changeColor);
-function changeColor() {
-    container.style.backgroundColor = colorBg.value;
-
-    for (item of headings) {
-        item.style.color = colorHeading.value;
-    }
-    for (item of paragraphs) {
-        item.style.color = colorParagraph.value;
-    }
+function addTask() {
+    const task = document.createElement('li');
+    task.textContent = newTask.value;
+    tasks.appendChild(task);
 }
 
-
-/*
-Fonas
-h teksto spalva
-p teksto spalva
- */
+function deleteTheTask() {
+    const lastTask = document.querySelector('li:last-child');
+    tasks.removeChild(lastTask);
+}
